28b0725d829811cf322af5f28416af92798f6d16
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-06-10 10:16:06 -0300

    React/TS Learning Day 28 - ContextAPI

---
+++ src/testfiles/day28.tsx
@@ -0,0 +1,24 @@
+import React, {useState, useContext} from "react";
+ interface ThemeContextType{
+    theme: 'light' | 'dark';
+    toggleTheme: () => void;
+ }
+
+ export const ThemeContext = React.createContext<ThemeContextType | undefined>(undefined);
+
+ interface ThemeProviderProps {
+    children: React.ReactNode;
+ }
+
+ expost const ThemeProvider: ({children}:ThemeProviderProps) => {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+    const toggleTheme = () => { 
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));   
+ } ;
+
+ return (
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+        {children}
+    </ThemeContext.Provider>
+ );
