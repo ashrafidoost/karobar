0b60df65e7a378de46076f8c2f39083ab5b4fc53
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-06-09 21:47:07 -0300

    React/TS Learning Day 27 - Deep Nav

--- src/testfiles/day27.tsx
+++ src/testfiles/day27.tsx
@@ -3,3 +3,11 @@ import React, { usestate, useEffect } from "react";
 interface User {
   userId: number;
 }
+
+function Myfunc() {
+  return (
+    <div>
+      <h1>Myfunc Component</h1>
+    </div>
+  );
+}
